6d70eb0dd954192c17f205daa23413673692fe97
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-05-09 12:42:16 -0300

    Add score file to modules folder and some basic styling

---
+++ src/modules/scores.js
@@ -0,0 +1,24 @@
+const scores = [
+  {
+    name: 'Sebastian',
+    score: 200,
+  },
+  {
+    name: 'John',
+    score: 50,
+  },
+  {
+    name: 'Nan',
+    score: 150,
+  },
+  {
+    name: 'Joshep',
+    score: 250,
+  },
+  {
+    name: 'Genoveba',
+    score: 100,
+  },
+];
+
+export default scores;
